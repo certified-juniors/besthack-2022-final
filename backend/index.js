d6568d9f466e41ebe7e6db812ea091d5b228b198
cry1s <cry1s@ya.ru>
2022-04-23 11:50:24 +0300

    back init

---
+++ backend/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const routes = require('./routes');
+const bodyparser = require('body-parser');
+const { db } = require('./config');
+
+const app = express();
+
+app.use(bodyparser.urlencoded({ extended: false }));
+app.use(express.json())
+app.use(routes);
+
+const PORT = process.env.PORT || 2001;
+
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+});
