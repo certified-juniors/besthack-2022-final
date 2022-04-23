8bcef015c112240c906fb6eb4fd605e3366980a9
cry1s <cry1s@ya.ru>
2022-04-23 22:00:40 +0300

    ok

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,10 @@ const app = express();
 app.use(bodyparser.urlencoded({ extended: false }));
 app.use(express.json())
 app.use(routes);
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    return next()
+});
 
 const PORT = process.env.PORT || 2001;
 
