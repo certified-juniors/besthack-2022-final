a59d5570e419ae923db06c70a8e46f38cd1690a7
Vanv1k <ivanspecial5@gmail.com>
2022-04-23 20:52:49 +0300

    try to get data

--- frontend/src/Components/Currency.jsx
+++ frontend/src/Components/Currency.jsx
@@ -2,61 +2,67 @@ import React from 'react';
 import {Card} from "react-bootstrap";
 import {useState} from "react";
 import CurrencyList from "./CurrencyList";
+import axios from "axios";
 
+const myaxios = axios.create({
+  baseURL: 'http://127.0.0.1:2001',
+  method: "POST",
+});
 
+const Currency = () => {
 
-const Currency = () =>
-{
-  const [myCurrency, setMyCards] = useState( [
+  const [myCurrency, setMyCards] = useState([
     {
       title: 'Currency num.1',
-      img : ' ',
+      img: ' ',
       text: 'exchange rate 1',
     },
     {
       title: 'Currency num.2',
-      img : ' ',
+      img: ' ',
       text: 'exchange rate 2',
     },
     {
-      title : 'Currency num.3',
-      img : ' ',
+      title: 'Currency num.3',
+      img: ' ',
       text: 'exchange rate 3',
     },
     {
-      title : 'Currency num.4',
-      img : ' ',
+      title: 'Currency num.4',
+      img: ' ',
       text: 'exchange rate 4',
     },
     {
-      title : 'Currency num.5',
-      img : ' ',
+      title: 'Currency num.5',
+      img: ' ',
       text: 'exchange rate 5',
     },
     {
-      title : 'Currency num.6',
-      img : ' ',
+      title: 'Currency num.6',
+      img: ' ',
       text: 'exchange rate 6'
     }
   ])
-
-  const currency_list = myCurrency.map( (card) =>
-
-     <Card>
-       <Card.Body>
-         <Card.Title>{card.title}</Card.Title>
-         <Card.text>{card.text}</Card.text>
-       </Card.Body>
-
-     </Card>
-
+  myaxios("last-exchange-rates", {method: "POST"}).then(
+    res => {
+      setMyCards(res.data);
+    }
   )
+  const currency_list = myCurrency.map((card) =>
 
+    <Card>
+      <Card.Body>
+        <Card.Title>{card.title}</Card.Title>
+        <Card.text>{card.text}</Card.text>
+      </Card.Body>
+
+    </Card>
+  )
 
 
   return (
     <div>
-      <CurrencyList myCurrency = {myCurrency}/>
+      <CurrencyList myCurrency={myCurrency}/>
     </div>
   )
 }
