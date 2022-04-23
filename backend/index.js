d8ca8a258ab25d4c796ad6c909b8d2a8c51b8b67
Azat-Bilalov <52991059+Azat-Bilalov@users.noreply.github.com>
2022-04-23 12:15:10 +0300

    routes

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const routes = require('./routes');
 const bodyparser = require('body-parser');
-const { db } = require('./config');
 
 const app = express();
 
---
+++ backend/routes/index.js
@@ -0,0 +1,11 @@
+const express = require('express'),
+  router = express.Router()
+
+// тест запрос
+router.route('/').get(async (req, res) => {
+  return res
+    .status(200)
+    .send('<h1>Hello!</h1>')
+})
+
+module.exports = router
