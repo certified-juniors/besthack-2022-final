187a425c14efe6abdfe90ba3d7f7302457e16a9e
cry1s <cry1s@ya.ru>
2022-04-23 12:30:54 +0300

    user and article models

---
+++ backend/models/Article.js
@@ -0,0 +1,10 @@
+class Article {
+    constructor(id, title, content, source, created_at, updated_at) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.source = source;
+        this.created_at = created_at;
+        this.updated_at = updated_at;
+    }
+}
---
+++ backend/models/User.js
@@ -0,0 +1,11 @@
+class User {
+    constructor(id, login, email, hashed_password, created_at, updated_at, is_admin) {
+        this.id = id;
+        this.login = login;
+        this.email = email;
+        this.hashed_password = hashed_password;
+        this.created_at = created_at;
+        this.updated_at = updated_at;
+        this.is_admin = is_admin;
+    }
+}
