d28b468a81eee275d53c7abd4889a4c30f482c66
cry1s <cry1s@ya.ru>
2022-04-23 13:02:37 +0300

    json

--- backend/models/Article.js
+++ backend/models/Article.js
@@ -1,17 +1,24 @@
 class Article {
-    constructor(id, title, content, source, created_at, updated_at) {
+    CATEGORY = {
+        FOREX: 1,
+        SECMARKS: 2,
+        POLICY: 3,
+    }
+
+    constructor(id, title, content, source, created_at) {
         this.id = id;
         this.title = title;
         this.content = content;
         this.source = source;
         this.created_at = created_at;
-        this.updated_at = updated_at;
     }
-}
 
-class Category extends Enumerator {
-    constructor(id, name, created_at, updated_at) {
-        super(id, name, created_at, updated_at);
+    constructor(json) {
+        this.id = json.id;
+        this.title = json.title;
+        this.content = json.content;
+        this.source = json.source;
+        this.created_at = json.created_at;
     }
 }
 
--- backend/models/User.js
+++ backend/models/User.js
@@ -1,14 +1,25 @@
 class User {
-    constructor(id, login, email, hashed_password, created_at, updated_at, is_admin) {
+    ROLE = {
+        ADMIN: 'admin',
+        READONLY: 'readonly',
+        CLIENT: 'client',
+    };
+    constructor(id, login, email, hashed_password, created_at, role) {
         this.id = id;
         this.login = login;
         this.email = email;
         this.hashed_password = hashed_password;
         this.created_at = created_at;
-        this.updated_at = updated_at;
-        this.is_admin = is_admin;
+        this.role = role;
+    }
+    constructor(json) {
+        this.id = json.id;
+        this.login = json.login;
+        this.email = json.email;
+        this.hashed_password = json.hashed_password;
+        this.created_at = json.created_at;
+        this.role = json.role;
     }
 }
-
 
 module.exports = User;
