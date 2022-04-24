dae0d9a67f88077af17eb2a3d19e00067c4503b1
Azat-Bilalov <52991059+Azat-Bilalov@users.noreply.github.com>
2022-04-24 08:20:58 +0300

    added flags to cards

--- frontend/src/Components/CurrencyList.jsx
+++ frontend/src/Components/CurrencyList.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import {Button, Container, Card, Row, Col} from "react-bootstrap";
+import {Button, Container, Card, Row, Col, Image} from "react-bootstrap";
 import Currency from "./Currency";
 import ModalButton from "./ModalButton";
 
-
+import Rus from '../flags/Rus.jpg'
 
 const CurrencyList = (massCurrency) => {
   return (
@@ -15,13 +15,19 @@ const CurrencyList = (massCurrency) => {
             <Container fluid style={{marginTop: '30px', marginLeft: '15px', padding: '15px'}}>
 
               <Card style={{width: '25rem', height: '10rem'}}>
-                <Card.Img variant="top" src={myCurrency.img}/>
                 <Card.Body>
                   <Card.Title style = {{marginTop: '6px'}}>{myCurrency.title}</Card.Title>
-                  <Card.Text>
-                   Текущий курс: {myCurrency.text}
-                  </Card.Text>
-                  {myCurrency.title == "RUB" ? "" : <ModalButton myCurrency={myCurrency}/>}
+                  <div className="row g-0">
+                    <div className="col-8">
+                      <Card.Text>
+                       Текущий курс: {myCurrency.text}
+                      </Card.Text>
+                      {myCurrency.title === "RUB" ? "" : <ModalButton myCurrency={myCurrency}/>}
+                    </div>
+                    <div className="col-4">
+                      <Image fluid src={myCurrency.img} className="rounded-start" />
+                    </div>
+                  </div>
                 </Card.Body>
               </Card>
             </Container>
