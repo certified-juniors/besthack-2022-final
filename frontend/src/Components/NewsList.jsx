9207eba4310443ad25480db529a6388b5a99148e
SicParv1sMagna <92353726+SicParv1sMagna@users.noreply.github.com>
2022-04-24 04:28:30 +0300

    news commit

--- frontend/src/Components/NewsList.jsx
+++ frontend/src/Components/NewsList.jsx
@@ -4,29 +4,29 @@ import ModalButtonNews from "./ModalButtonNews"
 
 const NewsList = (massNews) => {
 
-  function padTo2Digits(num) {
-    return num.toString().padStart(2, '0');
-  }
+    function padTo2Digits(num) {
+        return num.toString().padStart(2, '0');
+    }
 
-  function formatDate(date) {
-    return [
-      padTo2Digits(date.getDate()),
-      padTo2Digits(date.getMonth() + 1),
-      date.getFullYear(),
-    ].join('/');
-  }
+    function formatDate(date) {
+        return [
+            padTo2Digits(date.getDate()),
+            padTo2Digits(date.getMonth() + 1),
+            date.getFullYear(),
+        ].join('/');
+    }
 
 // 👇 24/10/2021 (mm/dd/yyyy)
 
 
-    let items = [];
-    for (let number = 1; number <= 20; number++) {
-        items.push(
-            <Pagination.Item key={number}>
-                {number}
-            </Pagination.Item>,
-        );
-    }
+    // let items = [];
+    // for (let number = 1; number <= 20; number++) {
+    //     items.push(
+    //         <Pagination.Item key={number}>
+    //             {number}
+    //         </Pagination.Item>,
+    //     );
+    // }
 
     const [show, setShow] = useState(false);
 
@@ -39,27 +39,38 @@ const NewsList = (massNews) => {
 
                 {massNews.myNews.map(myNews => (
 
-                    <Col sm="4">
-                        <Container fluid style={{marginTop: '50px', marginLeft: '50px'}}>
+                    <Col lg-4>
+                        <Container fluid style={{marginTop: "20px"}}>
 
-                            <Card style={{width: '20em', height: '15em'}}>
+                            <Card style={{width: '26em', height: '15em'}}>
 
                                 <Card.Body style={{marginBottom: '10px'}}>
-                                    <Card.Title style = {{margin: "10px"}}>
+                                    <Card.Title style={{margin: "10px"}}>
                                         {myNews.title}
                                     </Card.Title>
-
-                                   <span> <ModalButtonNews myNews={myNews} /> <p style={{marginTop: '20px', MozTextSizeAdjust: 'sm'}}> {(formatDate(new Date(myNews.date)))}</p></span>
+                                    <div style = {{position: "absolute", bottom: "0"}}>
+                                        <Row>
+                                            <Col>
+                                                <p style={{
+                                                    marginTop: '10px',
+                                                    MozTextSizeAdjust: 'sm'
+                                                }}> {(formatDate(new Date(myNews.date)))}</p>
+                                            </Col>
+                                            <Col style = {{marginLeft: '70px'}}>
+                                                <ModalButtonNews myNews={myNews} className = "justify-content-end"/>
+                                            </Col>
+                                        </Row>
+                                    </div>
                                 </Card.Body>
                             </Card>
                         </Container>
                     </Col>))
                 }
             </Row>
-            <Pagination className="justify-content-md-center"
-                        style={{marginTop: '15px', marginLeft: '15px', marginBottom: '15px'}}>
-                {items}
-            </Pagination>
+            {/*<Pagination className="justify-content-md-center"*/}
+            {/*            style={{marginTop: '15px', marginLeft: '15px', marginBottom: '15px'}}>*/}
+            {/*    {items}*/}
+            {/*</Pagination>*/}
         </>
     )
 }
